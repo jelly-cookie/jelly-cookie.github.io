6e840439abe002addcde03f8442cd72b63a4f4ee
jelly-cokie <jelly.gummy.fruit0@gmail.com>
2025-03-08 13:32:03 +0900

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_ALPHA_VANTAGE_API_KEY;
-const BASE_URL = 'https://www.alphavantage.co/query';
-
 export const fetchNews = async () => {
   try {
     const response = await axios.get('/news-data.json');
