1ae18d7fe7bb8a6c527ab1e3830ff541b8d6cfa7
jelly-cokie <jelly.gummy.fruit0@gmail.com>
2025-03-19 22:06:05 +0900

    Fix

--- src/components/EconomicTerms.js
+++ src/components/EconomicTerms.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import economicTermsData from '../data/economic-terms.json';
 
 const EconomicTerms = () => {
